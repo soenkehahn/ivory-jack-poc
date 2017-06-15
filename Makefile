68b1896d8e8b53b98c4b7c6b3fe70229b717ddc9
Sönke Hahn <soenkehahn@gmail.com>
2017-06-14 23:30:18 -0400

    add stack file and make compile again

--- Makefile
+++ Makefile
@@ -10,11 +10,12 @@ a.out: foo.o main.o
 foo.o: foo.c
 	gcc foo.c -o foo.o -c -O11
 
-foo.c: Main.hs
-	cabal exec -- runhaskell -Wall -Werror Main.hs
+foo.c: src/Main.hs
+	stack build
+	stack exec generate-foo
 
 main.o: main.c
 	gcc main.c -std=c99 -pedantic-errors -c -O11
 
 clean:
-	rm -rf a.out foo.* ivory.* ivory_asserts.h main.o
+	rm -rf a.out foo.* ivory.* ivory_asserts.h ivory_templates.h main.o
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,125 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE TypeFamilies      #-}
 
-module Main where
+module Main (main, add) where
+
+import           Control.Applicative
+import           Control.Arrow
+import           Control.Monad.State.Lazy
+import qualified Ivory.Compile.C.CmdlineFrontend as C
+import           Ivory.Language
+import           Ivory.Language.Module
 
 main :: IO ()
-main = return ()
+main = C.runCompiler [package "foo" fooModule] [] C.initialOpts{
+  C.outDir = Just "."
+}
+
+fooModule :: ModuleM ()
+fooModule = runWithUniqueNames $ do
+  rs <- forM (map (\ i -> 0.00002 * fromIntegral i) [1 .. 3 :: Integer]) $
+    \ inc -> do
+      lfo <- join (mult <$> (saw inc) <*> (constant 0.1))
+      rect lfo
+  siren <- join (mult <$> constant 0.3 <*> mix rs)
+  bassdrum <- do
+    kick <- rect =<< constant 0.00002
+    ramp <- saw 0.000001
+    mult kick ramp
+  mix [siren, bassdrum, bassdrum]
+
+
+type M a = StateT (Integer, [Def ('[] :-> ())]) ModuleM a
+
+runWithUniqueNames :: M (Signal IFloat) -> ModuleM ()
+runWithUniqueNames action = do
+  (outputSignal, (_counter, processors)) <- runStateT action (0, [])
+  jack_ivory_main processors outputSignal
+
+unique :: String -> M String
+unique pattern = do
+  (counter, processors) <- get
+  put (succ counter, processors)
+  return (pattern ++ "_" ++ show counter)
+
+addProcessor :: Def ('[] :-> ()) -> M ()
+addProcessor p =
+  modify (second (++ [p]))
+
+mkProcessor :: (IvoryInit a, IvoryZeroVal a, Num a) =>
+     String
+  -> (Signal a -> Body ())
+  -> M (Signal a)
+mkProcessor pattern action = do
+  outputRefName <- unique (pattern ++ "_output")
+  let outputRef = area outputRefName (Just (ival 0))
+
+  processorName <- unique (pattern ++ "_proc")
+  let processor = proc processorName $ action $ Signal $ addrOf outputRef
+  lift $ do
+    defMemArea outputRef
+    incl processor
+  addProcessor processor
+
+  return $ Signal $ addrOf outputRef
+
+mkState :: (Ref Global (Stored IFloat) -> M a) -> M a
+mkState action = do
+  phaseRefName <- unique "rect_phase"
+  let phaseRef = area phaseRefName (Just (ival 0))
+  lift $ defMemArea phaseRef
+  action $ addrOf phaseRef
+
+jack_ivory_main :: [Def ('[] :-> ())] -> Signal IFloat -> ModuleM ()
+jack_ivory_main processors (Signal input) =
+  incl $ proc "jack_ivory_main" $ body $ do
+    mapM_ call_ processors
+    x <- deref input
+    ret x
+
+data Signal a = Signal (Ref Global (Stored a))
+
+rect :: Signal IFloat -> M (Signal IFloat)
+rect (Signal inc) = mkState $ \ phaseRef -> mkProcessor "rect" $ \ (Signal output) -> body $ do
+  phase <- deref phaseRef
+  incV <- deref inc
+  let newPhase = (phase >=? 1) ? (phase - 1, phase)
+      newPhase' = newPhase + incV
+  store phaseRef newPhase'
+  store output ((newPhase' <? 0.5) ? (- 1, 1 :: IFloat))
+
+saw :: IFloat -> M (Signal IFloat)
+saw inc = mkState $ \ phaseRef -> mkProcessor "saw" $ \ (Signal output) -> body $ do
+  phase <- deref phaseRef
+  let newPhase = (phase >=? 1) ? (phase - 1, phase)
+      newPhase' = newPhase + inc
+  store phaseRef newPhase'
+  store output newPhase'
+
+add :: Signal IFloat -> Signal IFloat -> M (Signal IFloat)
+add (Signal a) (Signal b) = mkProcessor "add" $ \ (Signal output) ->
+  body $ do
+    r <- (+) <$> deref a <*> deref b
+    store output r
+
+mix :: [Signal IFloat] -> M (Signal IFloat)
+mix signals = mkProcessor "mix" $ \ (Signal out) -> body $ do
+    values <- mapM (\ (Signal ref) -> deref ref) signals
+    store out (sum values / len)
+  where
+    len :: IFloat
+    len = fromIntegral $ length signals
+
+constant :: (IvoryInit a, Num a, IvoryStore a, IvoryZeroVal a) =>
+  a -> M (Signal a)
+constant c = mkProcessor "constant" $ \ (Signal out) -> body $ do
+  store out c
+
+mult :: (IvoryInit a, Num a, IvoryStore a, IvoryZeroVal a) =>
+  Signal a -> Signal a -> M (Signal a)
+mult (Signal a) (Signal b) = mkProcessor "mult" $ \ (Signal out) -> body $ do
+  a' <- deref a
+  b' <- deref b
+  store out (a' * b')
